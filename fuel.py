0e89014bf3877c02314c4e780a6f59dda5dbf1c1
test <test>
2023-11-06 13:18:12 +0700

    test

---
+++ fuel.py
@@ -0,0 +1,20 @@
+r = True
+while r == True:
+    try:
+        frac = input('Дробь: ')
+        s = frac.split ('/')
+        X = int(s[0])
+        Y = int(s[1])
+        f = X/Y * 100
+        f //=1
+        r = False
+    except ValeError:
+        continue
+    except ZeroDivisionError:
+        continue
+if f >=99:
+    print('F')
+elif f <=1:
+    print('E')
+else:
+    print(str(int(f))+ '%')
